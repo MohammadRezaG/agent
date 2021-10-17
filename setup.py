d4be8973dba5a568173de82c095beb5f3846b1f1
Mohammad reza golsorkhi <mgol2077@outlook.com>
2021-10-17 14:53:36 +0330

    no_dill_error

--- setup.py
+++ setup.py
@@ -1,37 +1,4 @@
 from setuptools import setup
 
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-
-setup(
-    name='agent_Job_scheduler',
-    version='0.2.3',
-    packages=['agent'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.9',
-    ],
-    keywords='tasks jobs periodic task interval periodic_job periodicjob flask style decorator agent job scheduler time agent_Job agent_Job_scheduler',
-
-    project_urls={
-        'Documentation': 'https://github.com/MohammadRezaG/agent/wiki',
-        'Source': 'https://github.com/MohammadRezaG/agent',
-        'Tracker': 'https://github.com/MohammadRezaG/agent/issues',
-    },
-    install_requires=[
-    ],
-    license='Apache License 2.0',
-    author='Mohammad Reza Golsorkhi',
-    author_email='Mgol2077@outlook.com',
-    description='agent is a python job scheduler',
-    long_description=readme(),
-    python_requires='>=3.8',
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    include_package_data=True,
-    zip_safe=True
-)
+if __name__ == "__main__":
+    setup()
